cf738450d77c4fa57258a8e82e0be0ff5a430f02
milagrosmaurer <112497616+milagrosmaurer@users.noreply.github.com>
2023-06-16 16:31:39 -0300

    Update puesto_de_comida.h

--- src/puesto_de_comida.h
+++ src/puesto_de_comida.h
@@ -1,8 +1,40 @@
 #ifndef TP3_PUESTO_DE_COMIDA_H
 #define TP3_PUESTO_DE_COMIDA_H
 
+#include<iterator>
+#include <set>
+#include "tipos.h"
 
 class puesto_de_comida {
+public:
+    puesto_de_comida CrearPuesto(map<Producto ,int >& precios,map<Producto ,int >& stock, map<Producto , map <int , int>>& descuentos);
+
+    int obtenerStock(puesto_de_comida puesto,Producto item);
+
+    bool estaEnElMenu(puesto_de_comida puesto, Producto item);
+
+    int obtenerDescuento(puesto_de_comida puesto, Producto item, int cantidad);
+
+    int aplicarDescuento (int dinero, int cantidad);
+
+    int obtenerGasto(puesto_de_comida puesto, Persona a);
+
+    map<Producto , pair< int, int >>;
+
+    int obtenerCantVendidaSinDesc(puesto_de_comida puesto, Persona a, Producto item);
+
+    void registrarVenta(puesto_de_comida puesto, Persona a, Producto item, int cantidad);
+
+    void hackeoPuesto(puesto_de_comida puesto, Persona a, Producto item);
+
+private:
+
+    
+
+
+
+
+
 
 
 };
