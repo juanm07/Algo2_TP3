d7fec2db34740979ed762b3932cc53fa53d55d26
viaguirre <93285792+viaguirre@users.noreply.github.com>
2023-06-26 16:14:19 -0300

    Update lollapatuza.h

--- src/lollapatuza.h
+++ src/lollapatuza.h
@@ -5,35 +5,35 @@
 #include<iterator>
 #include <vector>
 #include "tipos.h"
-#include "puesto.h"
+#include "puesto.cpp"
 #include "ColaPrior.hpp"
 
 class Lollapatuza {
 public:
     Lollapatuza crearLolla(const map<IdPuesto, Puesto>& puestos, const vector<Persona>& personas);
 
-    void registrarCompra(Lollapatuza lolla, IdPuesto id, Persona a, Producto item, Cantidad cantidad);
+    void registrarCompra(IdPuesto id, Persona a, Producto item, Cantidad cantidad);
 
-    void hackear(Lollapatuza lolla, Persona a, Producto item);
+    void hackear(Persona a, Producto item);
 
-    Dinero gastoTotal(Lollapatuza lolla, Persona a);
+    Dinero gastoTotal(Persona a);
 
-    Persona personaQueMasGasto(Lollapatuza lolla);
+    Persona personaQueMasGasto();
 
-    IdPuesto menorStock(Lollapatuza lolla, Producto item);
+    IdPuesto menorStock(Producto item);
 
-    set<Persona> obtenerPersonas(Lollapatuza lolla);
+    vector<Persona> obtenerPersonas();
 
-    map<IdPuesto, Puesto> obtenerPuesto(Lollapatuza lolla);
+    map<IdPuesto, Puesto> obtenerPuesto();
 
 private:
     vector<Persona> _personas;
     map<IdPuesto, Puesto> _puestos;
     Persona _mayorConsumidora;
     map<Persona, map<Producto, Cantidad>> _consumosPorPersona;
-    map<Persona, map<Producto, map<IdPuesto, *Puesto>>> _puestosHackeables;
-    map<Producto, Nat> _precios;
-    ColaPrior<<Nat, iterator>>  _gastosPorPersona;
+    map<Persona, map<Producto, map<IdPuesto, Puesto*>>> _puestosHackeables;
+    map<Producto, Dinero> _precios;
+    ColaPrior<pair<Dinero, map<Persona,Indice>::iterator>>  _gastosPorPersona;
     map<Persona, Indice> _personasEnGasto;
 };
 
