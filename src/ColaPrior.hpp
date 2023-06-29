4fc63f46fb31c543dfe98cee312ecd58d768a306
viaguirre <93285792+viaguirre@users.noreply.github.com>
2023-06-29 14:38:29 -0300

    Update ColaPrior.hpp

--- src/ColaPrior.hpp
+++ src/ColaPrior.hpp
@@ -1,16 +1,15 @@
 #include "ColaPrior.h"
-
 template<class T>
 ColaPrior<T>::ColaPrior(): heap() {}
 
 template<class T>
 int ColaPrior<T>::tam() const {
-	return heap.size();
+    return heap.size();
 }
 
 template<class T>
 void ColaPrior<T>::encolar(const pair<int,map<int,int>::iterator>& elem) {
-	heap.push_back(elem);
+    heap.push_back(elem);
     int n = heap.size()-1;
     while (n > 0 && heap[padre(n)].first < heap[n].first){
         swap(heap[padre(n)],heap[n]);
@@ -20,12 +19,12 @@ void ColaPrior<T>::encolar(const pair<int,map<int,int>::iterator>& elem) {
 
 template<class T>
 const pair<int,map<int,int>::iterator>& ColaPrior<T>::proximo() const {
-	return heap[0];
+    return heap[0];
 }
 
 template<class T>
 void ColaPrior<T>::desencolar() {
-	//Bajo la raiz
+    //Bajo la raiz
     heap[0] = heap.back();
     heap.pop_back();
     heapifyDown(0);
@@ -33,7 +32,7 @@ void ColaPrior<T>::desencolar() {
 
 template<class T>
 ColaPrior<T>::ColaPrior(const vector<pair<int,map<int,int>::iterator>>& elems) {
-	heap = elems;
+    heap = elems;
     int _size = heap.size();
     for(int i = (_size/2)-1;i>=0;i--){
         heapifyDown(i);
@@ -51,7 +50,7 @@ void ColaPrior<T>::heapifyDown(int indice) {
         masGrande = hijoIzq;
     else{
         if (hijoIzq < size && heap[hijoIzq].first == heap[masGrande].first
-        && heap[hijoIzq].second->first > heap[masGrande].second->first){
+            && heap[hijoIzq].second->first > heap[masGrande].second->first){
             masGrande = hijoIzq;
         }
     }
@@ -65,7 +64,7 @@ void ColaPrior<T>::heapifyDown(int indice) {
     }
 
     if (masGrande != indice) {
-	heap[indice].second->second = masGrande; //Actualizo los indices en el diccionario
+        heap[indice].second->second = masGrande; //Actualizo los indices en el diccionario
         heap[masGrande].second->second = indice; //Actualizo los indices en el diccionario
         std::swap(heap[indice], heap[masGrande]);
         heapifyDown(masGrande);
@@ -74,27 +73,26 @@ void ColaPrior<T>::heapifyDown(int indice) {
 
 template<class T>
 void ColaPrior<T>::heapifyUp(int indice) {
-	while(indice > 0){
-	int padre = (indice - 1)/2;
-	if(heap[indice].first > heap [padre].first){
-	    heap[indice].second->second = padre; //Actualizo los indices en el diccionario
-	    heap[padre].second->second = indice; //Actualizo los indices en el diccionario
-	    swap(heap[indice],heap[padre]);
-	}else{
-	    if(heap[indice].first == heap[padre].first &&
-	       heap[indice].second->first > heap[padre].second->first){
-		heap[indice].second->second = padre; //Actualizo los indices en el diccionario
-		heap[padre].second->second = indice; //Actualizo los indices en el diccionario
-		swap(heap[indice],heap[padre]);
-	    }
-	    else{
-		break;
-	    }
-	}
-	indice = padre;
-	}
+    while(indice > 0){
+        int padre = (indice - 1)/2;
+        if(heap[indice].first > heap [padre].first){
+            heap[indice].second->second = padre; //Actualizo los indices en el diccionario
+            heap[padre].second->second = indice; //Actualizo los indices en el diccionario
+            swap(heap[indice],heap[padre]);
+        }else{
+            if(heap[indice].first == heap[padre].first &&
+               heap[indice].second->first > heap[padre].second->first){
+                heap[indice].second->second = padre; //Actualizo los indices en el diccionario
+                heap[padre].second->second = indice; //Actualizo los indices en el diccionario
+                swap(heap[indice],heap[padre]);
+            }
+            else{
+                break;
+            }
+        }
+        indice = padre;
+    }
 }
-
 template<class T>
 int ColaPrior<T>::padre(int n) {
     int res;
